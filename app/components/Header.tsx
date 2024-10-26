12659797d5539d5617a218027d0b5ba0dd04082a
Andres Gutierrez <agutierrez.codes@gmail.com>
2024-10-26 17:47:32 +0200

    refactor: format document

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,25 +7,25 @@ import { usePathname } from 'next/navigation'
 
 const navItems = [
   {
-    name: "Cafés",
+    name: 'Cafés',
     href: '/coffee'
   },
   {
-    name: "Panadería",
+    name: 'Panadería',
     href: '/bakery'
   },
   {
-    name: "Pastelería",
+    name: 'Pastelería',
     href: '/pastry'
   },
   {
-    name: "Tartas enteras",
+    name: 'Tartas enteras',
     href: '/pdfs/cakes'
   },
   {
-    name: "Contacto",
+    name: 'Contacto',
     href: '/contact'
-  },
+  }
 ]
 
 export default function Header() {
@@ -39,76 +39,80 @@ export default function Header() {
   const isHomePage = pathname === '/'
 
   return (
-    <nav
-      className={
-        isHomePage
-          ? 'absolute left-0 top-0 z-10 w-full bg-transparent text-white'
-          : 'relative w-full bg-white text-black shadow-md'
-      }
-    >
-      <div className='flex flex-wrap items-center justify-between p-2'>
-        <Link
-          href='/'
-          className='flex items-center space-x-3 md:hidden rtl:space-x-reverse'
-        >
-          <Image
-            src={isHomePage ? '/fans-logo-blanco.png' : '/fans-logo-oscuro.png'}
-            alt='Fans Logo'
-            height={200}
-            width={150}
-          />
-        </Link>
-        <button
-          onClick={toggleMenu}
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='text-gray-500 focus:ring-gray-200 h-10 w-10 rounded-lg p-2 text-sm hover:bg-transparent focus:outline-none focus:ring-2 md:hidden'
-          aria-controls='navbar-default'
-          aria-expanded={isMenuOpen}
-        >
-          <span className='sr-only'>Hamburger menu</span>
-          {isMenuOpen ? <IconBurger /> : <IconCross />}
-        </button>
-        <div
-          className={`w-full md:block ${isMenuOpen ? 'block' : 'hidden'}`}
-          id='navbar-default'
-        >
-          <ul className='mt-4 flex flex-col items-center justify-center rounded-lg text-xl font-medium backdrop-blur-md md:mt-0 md:flex-row md:space-x-16 md:text-base md:backdrop-blur-none rtl:space-x-reverse'>
-            <li onClick={toggleMenu} className='hidden md:block'>
-              <Link
-                href='/'
-                className='flex items-center space-x-3 rtl:space-x-reverse'
-              >
-                <Image
-                  src={
-                    isHomePage
-                      ? '/fans-logo-blanco.png'
-                      : '/fans-logo-oscuro.png'
-                  }
-                  className='h-15'
-                  alt='Fans Logo'
-                  width={200}
-                  height={200}
-                />
-              </Link>
-            </li>
-            {navItems.map(item => {
-              return (
-                <li key={item.href} onClick={toggleMenu}>
-                  <Link 
-                    href={item.href}
-                    className='text-gray-900 hover:bg-gray-100 block rounded px-3 py-2 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-green'
-                    aria-current={pathname === item.href}
-                  >
-                    {item.name}
-                  </Link>
-                </li>
-              )
-            })}
-          </ul>
+    <header>
+      <nav
+        className={
+          isHomePage
+            ? 'absolute left-0 top-0 z-10 w-full bg-transparent text-white'
+            : 'relative w-full bg-white text-black shadow-md'
+        }
+      >
+        <div className='flex flex-wrap items-center justify-between p-2'>
+          <Link
+            href='/'
+            className='flex items-center space-x-3 md:hidden rtl:space-x-reverse'
+          >
+            <Image
+              src={
+                isHomePage ? '/fans-logo-blanco.png' : '/fans-logo-oscuro.png'
+              }
+              alt='Fans Logo'
+              height={200}
+              width={150}
+            />
+          </Link>
+          <button
+            onClick={toggleMenu}
+            data-collapse-toggle='navbar-default'
+            type='button'
+            className='text-gray-500 focus:ring-gray-200 h-10 w-10 rounded-lg p-2 text-sm hover:bg-transparent focus:outline-none focus:ring-2 md:hidden'
+            aria-controls='navbar-default'
+            aria-expanded={isMenuOpen}
+          >
+            <span className='sr-only'>Hamburger menu</span>
+            {isMenuOpen ? <IconBurger /> : <IconCross />}
+          </button>
+          <div
+            className={`w-full md:block ${isMenuOpen ? 'block' : 'hidden'}`}
+            id='navbar-default'
+          >
+            <ul className='mt-4 flex flex-col items-center justify-center rounded-lg text-xl font-medium backdrop-blur-md md:mt-0 md:flex-row md:space-x-16 md:text-base md:backdrop-blur-none rtl:space-x-reverse'>
+              <li onClick={toggleMenu} className='hidden md:block'>
+                <Link
+                  href='/'
+                  className='flex items-center space-x-3 rtl:space-x-reverse'
+                >
+                  <Image
+                    src={
+                      isHomePage
+                        ? '/fans-logo-blanco.png'
+                        : '/fans-logo-oscuro.png'
+                    }
+                    className='h-15'
+                    alt='Fans Logo'
+                    width={200}
+                    height={200}
+                  />
+                </Link>
+              </li>
+              {navItems.map(item => {
+                return (
+                  <li key={item.href} onClick={toggleMenu}>
+                    <Link
+                      href={item.href}
+                      className='text-gray-900 hover:bg-gray-100 block rounded px-3 py-2 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-green'
+                      aria-current={pathname === item.href}
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   )
 }
 
@@ -122,7 +126,7 @@ function IconBurger() {
       className='h-5 w-5'
       aria-hidden='true'
       xmlns='http://www.w3.org/2000/svg'
-        fill='none'
+      fill='none'
       viewBox='0 0 24 24'
       stroke='currentColor'
     >
@@ -155,4 +159,3 @@ function IconCross() {
     </svg>
   )
 }
-
