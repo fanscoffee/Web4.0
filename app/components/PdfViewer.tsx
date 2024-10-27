a868e999e941d3d4166cef85aa11dade1e5568c6
Juan Rolo <juanluis211@gmail.com>
2024-10-27 18:49:22 +0100

    fix: delete unused hook and change pdfViewer

--- app/components/PdfViewer.tsx
+++ app/components/PdfViewer.tsx
@@ -1,16 +1,43 @@
 'use client'
 
-import  useWindowDimensions from './useWindowDimensions'
+import React, { useEffect, useState } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
 
 export default function PdfViewer(props: {pdf: string}) {
 const { height, width } = useWindowDimensions();
+console.log(height, " " , width)
   return (
       <iframe
         src={props.pdf}
         width="100%"
         height={height}
-        style={{ border: 'none',
-          overflow: "auto", }}
+        style={{ 
+          border: 'none',
+        }}
         title="PDF Viewer"
       />
   );
