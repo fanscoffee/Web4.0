d973e2f5d4e281c7df62a0820087e305ee46085d
Juan Rolo <juanluis211@gmail.com>
2025-03-07 00:39:22 +0100

    refactor: update JSON-LD structure and improve Tailwind CSS color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,11 +15,18 @@ const config: Config = {
         lg: '2rem',
       }
     },
-    screens: {
-      sm:'640px',
-      md:'768px',
-      lg:'960px',
-      xl:'1200px',
+    extend: {
+      colors: {
+        green: 'hsl(115, 35%, 43%)',
+        'dark-green': 'hsl(132, 38%, 18%)',
+        'light-yellow': 'hsl(64, 73%, 67%)',
+        yellow: 'hsl(63, 66%, 55%)',
+        brown: 'hsl(40, 87%, 15%)',
+        white: 'hsl(0, 0%, 100%)',
+        black: 'hsl(0, 0%, 0%)',
+        gray: 'hsl(0, 0%, 50%)',
+        beige: 'hsl(40, 33%, 84%)',
+      },
     },
   },
   plugins: [],
